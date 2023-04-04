ea0f5e3bd0ce3774ef68d02b10c841ddb286d83b
avrocoder <avrocoder@example.com>
2023-04-04 23:25:07 +0300

    Add Coordinates
    The class contains x and y coordinates, which correspond to cells on the battlefield

---
+++ src/main/java/com/github/avrocoder/Coordinates.java
@@ -0,0 +1,41 @@
+package com.github.avrocoder;
+
+import java.util.Objects;
+
+public class Coordinates {
+    private int x, y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
