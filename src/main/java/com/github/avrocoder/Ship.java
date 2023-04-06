a28a6b49ee81b8595390db0c3ca72bc7b3f0faa9
avrocoder <avrocoder@example.com>
2023-04-06 20:43:30 +0300

    Refactor Ship. Add decks and ship status control. Add ship orientation

--- src/main/java/com/github/avrocoder/Ship.java
+++ src/main/java/com/github/avrocoder/Ship.java
@@ -1,23 +1,47 @@
 package com.github.avrocoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ship {
-    private final Coordinates coordinates;
+    private final Coordinates headCoordinates;
     private final int size;
+    private final Orientation orientation;
     private ShipStatus status;
-    public Ship(Coordinates coordinates, int size) {
-        this.coordinates = coordinates;
+    private final Map<Coordinates, Deck> decks = new HashMap<>();
+    public Ship(Coordinates headCoordinates, int size, Orientation orientation) {
+        this.headCoordinates = headCoordinates;
         this.size = size;
+        this.orientation = orientation;
         this.status = ShipStatus.INTACT;
+        decksInit();
     }
-    public Coordinates getCoordinates() {
-        return coordinates;
+
+    private void decksInit() {
+        for (int i = 0; i < size; i++) {
+            decks.put(headCoordinates.shift(this.orientation, i), new Deck());
+        }
+    }
+
+    public Coordinates getHeadCoordinates() {
+        return headCoordinates;
     }
     public int getSize() {
         return size;
     }
     public ShipStatus getStatus() {
+        status = calculateStatus();
         return status;
     }
-    public void setStatus(ShipStatus status) {
-        this.status = status;
+    private ShipStatus calculateStatus() {
+        return ShipStatusManager.getCalculatedStatus(this);
+    }
+
+    public Map<Coordinates, Deck> getDecks() {
+        return decks;
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
     }
 }
