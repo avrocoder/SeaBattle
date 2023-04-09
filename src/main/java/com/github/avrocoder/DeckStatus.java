66920d8d7d6c072ae249af4fb71f9e4e592937a8
avrocoder <avrocoder@example.com>
2023-04-09 18:12:52 +0300

    Add deck status class

---
+++ src/main/java/com/github/avrocoder/DeckStatus.java
@@ -0,0 +1,11 @@
+package com.github.avrocoder;
+
+/**
+ * Status deck:
+ * INTACT - intact deck
+ * DAMAGED - damaged deck, but its ship isn't destroyed
+ * DESTROYED - damaged deck and destroyed ship
+ */
+public enum DeckStatus {
+    INTACT, DAMAGED, DESTROYED
+}
