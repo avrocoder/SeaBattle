160dd58b5a7cd81c95ccba859f09fec44dea2239
avrocoder <avrocoder@example.com>
2023-04-11 19:58:27 +0300

    Add test method to get deck by wrong number

--- src/test/java/com/github/avrocoder/ShipTest.java
+++ src/test/java/com/github/avrocoder/ShipTest.java
@@ -2,31 +2,22 @@ package com.github.avrocoder;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShipTest {
-    private final Object[][] configShips = {
-            {new Coordinates(3, 5), 3, Orientation.HORIZONTAL},
-            {new Coordinates(8, 6), 4, Orientation.VERTICAL},
-    };
     private Ship shipHorizontal;
     private Ship shipVertical;
     private final ShipStatus defaultShipStatus = ShipStatus.INTACT;
     @BeforeEach
     void setUp() {
-        shipHorizontal = new Ship((Coordinates) configShips[0][0], (Integer) configShips[0][1], (Orientation) configShips[0][2]);
-        shipVertical = new Ship((Coordinates) configShips[1][0], (Integer) configShips[1][1], (Orientation) configShips[1][2]);
+        shipHorizontal = new Ship(new Coordinates(3, 5), 3, Orientation.HORIZONTAL);
+        shipVertical = new Ship(new Coordinates(8, 6), 4, Orientation.VERTICAL);
     }
+
     @Test
     void getSize() {
-        int size = (Integer) configShips[0][1];
-        assertEquals(size, shipHorizontal.getSize());
-        size = (Integer) configShips[1][1];
-        assertEquals(size, shipVertical.getSize());
+        assertEquals(3, shipHorizontal.getSize());
+        assertEquals(4, shipVertical.getSize());
     }
     @Test
     void getStatus() {
@@ -35,28 +26,23 @@ class ShipTest {
     }
 
     @Test
-    void getHeadCoordinates() {
-        Coordinates coordinatesHead = (Coordinates) configShips[0][0];
-        assertEquals(coordinatesHead, shipHorizontal.getHeadCoordinates());
-        coordinatesHead = (Coordinates) configShips[1][0];
-        assertEquals(coordinatesHead, shipVertical.getHeadCoordinates());
+    void checkHeadCoordinatesShip() {
+        assertEquals(3, shipHorizontal.getHeadCoordinates().getX());
+        assertEquals(5, shipHorizontal.getHeadCoordinates().getY());
+    }
+
+    @Test
+    void checkCorrectCoordinatesDecksOnHorizontalShip() {
+        assertEquals(new Coordinates(3, 5), shipHorizontal.getMapEntryDeckByNumber(0).getKey());
+        assertEquals(new Coordinates(4, 5), shipHorizontal.getMapEntryDeckByNumber(1).getKey());
+        assertEquals(new Coordinates(5, 5), shipHorizontal.getMapEntryDeckByNumber(2).getKey());
     }
     @Test
-    void getDecks() {
-        Map<Coordinates, Deck> factDecks = shipHorizontal.getDecks();
-        Map<Coordinates, Deck> expectedDecks = new HashMap<>();
-        expectedDecks.put(new Coordinates(3, 5), new Deck(new Coordinates(3, 5),shipHorizontal));
-        expectedDecks.put(new Coordinates(4, 5), new Deck(new Coordinates(4, 5), shipHorizontal));
-        expectedDecks.put(new Coordinates(5, 5), new Deck(new Coordinates(5, 5), shipHorizontal));
-        assertEquals(factDecks,expectedDecks);
-
-        factDecks = shipVertical.getDecks();
-        expectedDecks = new HashMap<>();
-        expectedDecks.put(new Coordinates(8, 6), new Deck(new Coordinates(8, 6),shipVertical));
-        expectedDecks.put(new Coordinates(8, 7), new Deck(new Coordinates(8, 7),shipVertical));
-        expectedDecks.put(new Coordinates(8, 8), new Deck(new Coordinates(8, 8),shipVertical));
-        expectedDecks.put(new Coordinates(8, 9), new Deck(new Coordinates(8, 9),shipVertical));
-        assertEquals(factDecks,expectedDecks);
+    void checkCorrectCoordinatesDecksOnVerticalShip() {
+        assertEquals(new Coordinates(8, 6), shipVertical.getMapEntryDeckByNumber(0).getKey());
+        assertEquals(new Coordinates(8, 7), shipVertical.getMapEntryDeckByNumber(1).getKey());
+        assertEquals(new Coordinates(8, 8), shipVertical.getMapEntryDeckByNumber(2).getKey());
+        assertEquals(new Coordinates(8, 9), shipVertical.getMapEntryDeckByNumber(3).getKey());
     }
     @Test
     void testGetDefaultStatus() {
@@ -64,38 +50,33 @@ class ShipTest {
         assertEquals(defaultShipStatus, shipVertical.getStatus());
     }
     @Test
-    void testGetDamagedAndDestroyedHorizontalShipStatus() {
+    void damagedAndDestroyedHorizontalShipStatus() {
         //first deck damage
-        shipHorizontal.getDecks().get(shipHorizontal.getHeadCoordinates()).setDamaged();
+        shipHorizontal.getMapEntryDeckByNumber(0).getValue().setDamaged();
         assertEquals(ShipStatus.DAMAGED, shipHorizontal.getStatus());
-
         //second deck damage
-        shipHorizontal.getDecks().get(shipHorizontal.getHeadCoordinates().shiftX(1)).setDamaged();
+        shipHorizontal.getMapEntryDeckByNumber(1).getValue().setDamaged();
         assertEquals(ShipStatus.DAMAGED, shipHorizontal.getStatus());
-
         //third deck damage
-        shipHorizontal.getDecks().get(shipHorizontal.getHeadCoordinates().shiftX(2)).setDamaged();
+        shipHorizontal.getMapEntryDeckByNumber(2).getValue().setDamaged();
         //ship has destroyed
         assertEquals(ShipStatus.DESTROYED, shipHorizontal.getStatus());
     }
     @Test
-    void testGetDamagedAndDestroyedVerticalShipStatus() {
+    void damagedAndDestroyedVerticalShipStatus() {
         //first deck damage
-        shipVertical.getDecks().get(shipVertical.getHeadCoordinates()).setDamaged();
+        shipVertical.getMapEntryDeckByNumber(0).getValue().setDamaged();
         assertEquals(ShipStatus.DAMAGED, shipVertical.getStatus());
-
         //second deck damage
-        shipVertical.getDecks().get(shipVertical.getHeadCoordinates().shiftY(1)).setDamaged();
+        shipVertical.getMapEntryDeckByNumber(1).getValue().setDamaged();
         assertEquals(ShipStatus.DAMAGED, shipVertical.getStatus());
-
         //third deck damage
-        shipVertical.getDecks().get(shipVertical.getHeadCoordinates().shiftY(2)).setDamaged();
+        shipVertical.getMapEntryDeckByNumber(2).getValue().setDamaged();
         assertEquals(ShipStatus.DAMAGED, shipVertical.getStatus());
-
-        //fourth deck damage
-        shipVertical.getDecks().get(shipVertical.getHeadCoordinates().shiftY(3)).setDamaged();
+        //forth deck damage
+        shipVertical.getMapEntryDeckByNumber(3).getValue().setDamaged();
+        //ship has destroyed
         assertEquals(ShipStatus.DESTROYED, shipVertical.getStatus());
-
     }
 
     @Test
@@ -103,6 +84,9 @@ class ShipTest {
         assertEquals(Orientation.HORIZONTAL, shipHorizontal.getOrientation());
     }
 
-
+    @Test
+    void getDeckByWrongNumber() {
+        assertThrows(IndexOutOfBoundsException.class, () -> shipHorizontal.getMapEntryDeckByNumber(5));
+    }
 
 }
