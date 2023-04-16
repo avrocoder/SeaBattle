a499d0671ac33bbcbd0cf30e083be935e13a9616
avrocoder <avrocoder@example.com>
2023-04-16 22:40:34 +0300

    Remove unnecessary imports

--- src/main/java/com/github/avrocoder/Move.java
+++ src/main/java/com/github/avrocoder/Move.java
@@ -1,8 +1,5 @@
 package com.github.avrocoder;
 
-import com.github.avrocoder.validations.MoveValidation;
-import com.github.avrocoder.validations.Validation;
-
 public class Move {
     private final ShotsField shotsField;
     public Move(ShotsField shotsField) {
