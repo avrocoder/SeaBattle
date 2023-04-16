944f3e7b728d57cdde260d37562e7f0806f47e74
avrocoder <avrocoder@example.com>
2023-04-16 22:38:00 +0300

    Add validation for move

---
+++ src/test/java/com/github/avrocoder/validations/MoveValidationTest.java
@@ -0,0 +1,44 @@
+package com.github.avrocoder.validations;
+
+import com.github.avrocoder.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveValidationTest {
+    private final ShotsField shotsField = new ShotsField(10, 10);
+
+    @Test
+    void validateCorrectMove() {
+        Coordinates coordinates = new Coordinates(0, 0);
+        MoveValidation moveValidation = new MoveValidation(coordinates, shotsField);
+
+        boolean moveResult = moveValidation.validate();
+
+        assertTrue(moveResult);
+    }
+
+    @Test
+    void validateFailMoveNotEmptyCell() {
+        Move move = new Move(shotsField);
+        Coordinates coordinates = new Coordinates(0, 0);
+        move.make(coordinates);
+        MoveValidation moveValidation = new MoveValidation(coordinates, shotsField);
+
+        boolean moveResult = moveValidation.validate();
+
+        assertFalse(moveResult);
+    }
+
+    @Test
+    void validateFailMoveOutOfBounds() {
+        Move move = new Move(shotsField);
+        Coordinates coordinates = new Coordinates(10, 10);
+        move.make(coordinates);
+        MoveValidation moveValidation = new MoveValidation(coordinates, shotsField);
+
+        boolean moveResult = moveValidation.validate();
+
+        assertFalse(moveResult);
+    }
+}
