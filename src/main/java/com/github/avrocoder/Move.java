668188163217aa6a75dcfca04d71f2bbec25488d
avrocoder <avrocoder@example.com>
2023-04-16 22:36:38 +0300

    Refactor methods

--- src/main/java/com/github/avrocoder/Move.java
+++ src/main/java/com/github/avrocoder/Move.java
@@ -1,23 +1,17 @@
 package com.github.avrocoder;
 
 import com.github.avrocoder.validations.MoveValidation;
+import com.github.avrocoder.validations.Validation;
 
 public class Move {
-    private final Coordinates coordinates;
     private final ShotsField shotsField;
-    private final MoveValidation moveValidation;
-    public Move(Coordinates coordinates, ShotsField shotsField) {
-        this.coordinates = coordinates;
+    public Move(ShotsField shotsField) {
         this.shotsField = shotsField;
-        this.moveValidation = new MoveValidation(coordinates, shotsField);
     }
 
-    public boolean make() {
-        if(moveValidation.validate()) {
-            shotsField.place(coordinates, new Shot(coordinates));
-            return true;
-        }
-        throw new RuntimeException("Incorrect move");
+    public void make(Coordinates coordinates) {
+        shotsField.place(coordinates, new Shot(coordinates));
     }
+
 
 }
