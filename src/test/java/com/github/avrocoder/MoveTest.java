3ee07f941e709aa5c6cbc207c9d55d1e934b31db
avrocoder <avrocoder@example.com>
2023-04-16 22:36:04 +0300

    Refactor methods

--- src/test/java/com/github/avrocoder/MoveTest.java
+++ src/test/java/com/github/avrocoder/MoveTest.java
@@ -9,40 +9,19 @@ class MoveTest {
     private ShotsField shotsField;
     @BeforeEach
     void setUp() {
-        shotsField = new ShotsField(8, 12);
+        shotsField = new ShotsField(10, 10);
     }
 
     @Test
-    void MoveToEmptyCorrectCell() {
-        Coordinates coordinates = new Coordinates(1, 1);
-        Move move = new Move(coordinates, shotsField);
+    void checkExistenceShotOnField() {
+        Move move = new Move(shotsField);
+        Coordinates coordinates = new Coordinates(0, 0);
+        move.make(coordinates);
 
-        boolean result = move.make();
+        Shot shot = shotsField.getField().get(coordinates);
 
-        assertTrue(result);
+        assertEquals(coordinates, shot.getCoordinates());
     }
 
-    @Test
-    void MoveToOccupiedCell() {
-        Coordinates coordinates = new Coordinates(1, 1);
-        Move move = new Move(coordinates, shotsField);
-
-        move.make();
-
-        assertThrows(RuntimeException.class, move::make);
-    }
-
-    @Test
-    void shotToOutOfBoundsFieldWidth() {
-        Coordinates shotCoordinate = new Coordinates(8, 11);
-
-        assertThrows(RuntimeException.class, () -> new Move(shotCoordinate, shotsField).make());
-    }
-    @Test
-    void shotToOutOfBoundsFieldHeight() {
-        Coordinates shotCoordinate = new Coordinates(7, 12);
-
-        assertThrows(RuntimeException.class, () -> new Move(shotCoordinate, shotsField).make());
-    }
 
 }
