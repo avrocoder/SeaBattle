3192e41eec2bb80471c573fe760deb24b778feb9
avrocoder <avrocoder@example.com>
2023-04-23 01:35:18 +0300

    Refactor move

--- src/main/java/com/github/avrocoder/Move.java
+++ src/main/java/com/github/avrocoder/Move.java
@@ -1,13 +1,25 @@
 package com.github.avrocoder;
 
-public class Move {
+    public class Move {
     private final ShotsField shotsField;
-    public Move(ShotsField shotsField) {
+    private final ShipsField shipsField;
+    private boolean hit = false;
+    public Move(ShotsField shotsField, ShipsField shipsField) {
         this.shotsField = shotsField;
+        this.shipsField = shipsField;
     }
 
-    public void make(Coordinates coordinates) {
+        public boolean isHit() {
+            return hit;
+        }
+
+        public void make(Coordinates coordinates) {
         shotsField.place(coordinates, new Shot(coordinates));
+        Deck deck = shipsField.getField().get(coordinates);
+        if(shipsField.getField().containsKey(coordinates)) {
+            deck.setDamaged();
+            hit = true;
+        }
     }
 
 
